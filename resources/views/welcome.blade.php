8f3bc83351152a736d531d29f590679f94e64caf
Marcos Leandro Bonilla Borges <85517789+Max29xD@users.noreply.github.com>
2024-07-10 20:06:37 +0000

    add  Content-Security-Policy tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        
 
-        <title>Laravel</title>
+        <title>SIS2 - RRHH</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
